a48178cd96246ef2181555adaf8aa30f0328030c
wadedo <60915311+wadedo@users.noreply.github.com>
2024-01-18 00:03:06 -0800

    Update GenreList.tsx
    filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,10 +7,14 @@ import {
   Spinner,
   Text,
 } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (isLoading) return <Spinner></Spinner>;
   if (error) return null;
@@ -25,7 +29,11 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             ></Image>
-            <Button fontSize="lg" variant="link">
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="link"
+            >
               {genre.name}
             </Button>
           </HStack>
