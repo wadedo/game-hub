3fe0a863acc6e7c59627fa23727681f7bfb4d066
wadedo <60915311+wadedo@users.noreply.github.com>
2024-01-27 23:35:02 -0800

    Update image-url.ts
    sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url:string)=>{
+  if (!url) return '';// 返回一个空的图片，此为临时过渡
   const target = 'media/';  
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' +url.slice(index);
