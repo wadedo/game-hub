f0ad462317f929dda1448ce10f8c6f38450c167f
wadedo <60915311+wadedo@users.noreply.github.com>
2024-01-28 23:22:03 -0800

    Update image-url.ts
    handle games with no image.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url:string)=>{
-  if (!url) return '';// 返回一个空的图片，此为临时过渡
+  if (!url) return noImage;
   const target = 'media/';  
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' +url.slice(index);
